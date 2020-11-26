a86a1310a2987933baadb88afec791223d40128d
Natasha McIntyre <natashamc.cs@gmail.com>
2020-11-26 15:15:51 +0000

    raise exception for full dockingstation

--- Solution5/lib/dockingstation.rb
+++ Solution5/lib/dockingstation.rb
@@ -4,11 +4,13 @@ class DockingStation
 attr_reader :bike
 
   def release_bike
-    @bike = Bike.new
+    fail "No bikes available" unless @bike
+    @bike
   end
 
   def dock(bike)
-    @bike = bike 
+    fail "Docking station is full" if @bike
+    @bike = bike
   end
 
 
--- Solution5/spec/docking_station_spec.rb
+++ Solution5/spec/docking_station_spec.rb
@@ -8,6 +8,10 @@ describe DockingStation do
     it 'It should show true' do
       expect(bike.working?).to eq true
     end
+
+    it 'raises error when no bikes available' do
+      expect { subject.release_bike }.to raise_error "No bikes available"
+    end
   end
 
   it {is_expected.to respond_to(:dock).with(1).argument}
@@ -22,4 +26,12 @@ describe DockingStation do
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
+
+  describe '#dock(bike)' do
+    it 'raises error when docking station is full' do
+      subject.dock(bike)
+      expect { subject.dock(Bike.new) }.to raise_error "Docking station is full"
+    end
+  end
+
 end
