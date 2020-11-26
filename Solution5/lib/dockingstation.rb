6a665344f0b0c3d573e7a80104ea1d1fbd576720
Natasha McIntyre <natashamc.cs@gmail.com>
2020-11-26 16:21:16 +0000

    create capacity constant

--- Solution5/lib/dockingstation.rb
+++ Solution5/lib/dockingstation.rb
@@ -1,21 +1,31 @@
 require_relative '../lib/bike.rb'
 class DockingStation
 
+DEFAULT_CAPACITY = 20
+
 attr_reader :bikes
   def initialize
     @bikes = []
   end
 
   def release_bike
-    fail "No bikes available" if @bikes.empty?
+    fail "No bikes available" if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail "Docking station is full" if @bikes.count >= 20
+    fail "Docking station is full" if full?
     @bikes << bike
   end
 
+  private
 
+  def full?
+    @bikes.count >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @bikes.empty?
+  end
 
 end
--- Solution5/spec/docking_station_spec.rb
+++ Solution5/spec/docking_station_spec.rb
@@ -19,17 +19,17 @@ describe DockingStation do
   it {is_expected.to respond_to(:bikes)}
 
   it "Docks the bike" do
-    expect(subject.dock(bike)).to eq bike 
+    expect(subject.dock(bike)).to eq [bike]
   end
 
   it 'returns docked bikes' do
     subject.dock(bike)
-    expect(subject.bikes).to eq bike
+    expect(subject.bikes).to eq [bike]
   end
 
   describe '#dock(bike)' do
     it 'raises error when docking station is full' do
-      20.times{subject.dock (Bike.new)}
+      DockingStation::DEFAULT_CAPACITY.times{subject.dock (Bike.new)}
       expect { subject.dock(Bike.new) }.to raise_error "Docking station is full"
     end
   end
